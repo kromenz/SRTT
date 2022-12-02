0f1946b712b3f013ea05d07f1f3672c4a366729d
DiogoBernardes <118618376+DiogoBernardes@users.noreply.github.com>
2022-12-02 18:27:01 +0000

    Create Project.java

---
+++ src/Project.java
@@ -0,0 +1,71 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Project implements Serializable{
+    private int idProjeto;
+    private String name;
+    private String client;
+    private float priceHour;
+    private ProjectState state;
+    private List<Tasks> tasks;
+
+    public Project(){this.tasks = new ArrayList<>();} //Construtor
+
+    public List<Tasks> getTasks(){return tasks;} //getter das tarefas
+
+    public void ListTasks(){
+        int numTasks = 1;
+        for(Tasks tk : this.tasks){
+            System.out.println("Id Tarefa: " + tk.getIdTask());
+            System.out.println("Data Inicio: " + tk.getTaskStartDate());
+            System.out.println("Data Fim: " + tk.getTaskEndDate());
+            System.out.println("Descrição: " + tk.getDescription());
+
+        }
+    }
+
+    public int getIdProjeto() {
+        return idProjeto;
+    }
+
+    public void setIdProjeto(int idProjeto) {
+        this.idProjeto = idProjeto;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public float getPriceHour() {
+        return priceHour;
+    }
+
+    public void setPriceHour(float priceHour) {
+        this.priceHour = priceHour;
+    }
+
+    public ProjectState getState() {
+        return state;
+    }
+
+    public void setState(ProjectState state) {
+        this.state = state;
+    }
+
+    public void setTasks(List<Tasks> tasks) {
+        this.tasks = tasks;
+    }
+}
