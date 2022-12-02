b09653a92e933be13e3f54e91133be2b9aef0140
DiogoBernardes <118618376+DiogoBernardes@users.noreply.github.com>
2022-12-02 14:54:43 +0000

    --

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        Repository repo;
+        Repository.deserialize("src\\data\\users.repo");
         LoginForm frameLogin = new LoginForm("SRTT");
         frameLogin.setVisible(true);
     }
--- src/Repository.java
+++ src/Repository.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class Repository implements Serializable{
 
@@ -12,5 +10,36 @@ public class Repository implements Serializable{
     private Map<String, Admin> admins = new HashMap<>();
     //private Map<String, Users> users = new HashMap<>();
 
+    public Repository(){};
+    public Map<String, Users> getUsers() {return users;}
+    public Map<String, Admin> getAdmins() {return admins;}
 
+    public void serialize(String filename){ /*Converter os objetos em bytes*/
+
+        try{
+            FileOutputStream fileOut = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(this);
+            out.close();
+            fileOut.close();
+            System.out.printf("Serialized data is saved in " + filename + "\n");
+        } catch(IOException ex) {
+            System.out.println("ErrorSerialize: " + ex.getMessage());
+        }
+    }
+
+    public static void deserialize(String filename){/*Converter os bytes em  objetos*/
+
+        try{
+            FileInputStream fileIn = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            repo = (Repository) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch(IOException ex){
+            System.out.println("ErrorDeserialize: " + ex.getMessage());
+        } catch(ClassNotFoundException ex){
+            System.out.println("Repository class not found. " + ex.getMessage());
+        }
+    }
 }
